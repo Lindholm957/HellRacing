f79c3b1d75c568c2af01bfeda96e260bf071ac8b
lindholm <yan957@gmail.com>
2023-09-28 18:08:28 +0300

    add player managers instantiations in game scenes

---
+++ Assets/Scripts/Server/RoomManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Server
+{
+    [RequireComponent(typeof(PhotonView))]
+    public class RoomManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+        {
+            if (scene.buildIndex == 1)
+            {
+                string prefabName = Path.Combine("Prefabs", "Photon", "PlayerManager");
+                PhotonNetwork.Instantiate(prefabName, Vector3.zero, Quaternion.identity);
+            }
+        }
+
+        public void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+}
